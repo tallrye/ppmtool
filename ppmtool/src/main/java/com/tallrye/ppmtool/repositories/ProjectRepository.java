db8dedeefd5f5f42b62a5ff00dd52f0f165dd2ec
tallrye <s_uzuncavdar@hotmail.com>
2019-01-07 23:40:35 +0300

    Project Repository

---
+++ ppmtool/src/main/java/com/tallrye/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.tallrye.ppmtool.repositories;
+
+import com.tallrye.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
